a970af69dc8d7160c8eccdda754e2837799c20f0
lxl6125 <lxl6125@yeah.net>
2023-12-10 19:47:37 +0800

    update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -2,7 +2,7 @@
 mkdir -p files/etc/openclash/core
 
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep ${1} | awk -F '"' '{print $4}' | grep -v "v3" )
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium?ref=core | grep download_url | grep ${1} | awk -F '"' '{print $4}' | grep -v "v3" )
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 
 wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
