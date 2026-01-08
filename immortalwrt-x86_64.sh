e2ed27a07aaa1446655263835465ee4a917abaee
lxl6125 <lxl6125@yeah.net>
2026-01-08 11:13:48 +0800

    Updated on 2026-01-08

--- immortalwrt-x86_64.sh
+++ immortalwrt-x86_64.sh
@@ -22,13 +22,7 @@ sed -i '/CONFIG_KERNEL_BUILD_DOMAIN/d' .config &&
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/custom/passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/custom/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/sing-box
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/xray-core
-
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadow-tls,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 
 # 替换 OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash package/custom/luci-app-openclash
@@ -46,4 +40,3 @@ rm -rf feeds/packages/net/openlist
 git clone --depth=1 https://github.com/lxl6125/openwrt-qbittorrent-enhanced package/custom/qbittorrent-enhanced
 rm -rf feeds/luci/applications/luci-app-qbittorrent
 rm -rf feeds/packages/net/qBittorrent-Enhanced-Edition
-
